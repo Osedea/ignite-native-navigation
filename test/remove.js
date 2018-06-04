7ba3748bfd6cb43940a4ec4d0170103398a5d42e
Evan Glicakis <evan.glicakis@osedea.com>
2018-06-04 17:24:25 -0400

    added eslint

--- test/remove.js
+++ test/remove.js
@@ -1,17 +1,18 @@
-const test = require('ava')
-const sinon = require('sinon')
-const plugin = require('../plugin')
+const test = require('ava');
+const sinon = require('sinon');
+const plugin = require('../plugin');
 
-test('removes NativeNavigation', async t => {
-  const removeModule = sinon.spy()
-  const removePluginComponentExample = sinon.spy()
+test('removes NativeNavigation', async (t) => {
+    const removeModule = sinon.spy();
+    const removePluginComponentExample = sinon.spy();
 
-  const context = {
-    ignite: { removeModule, removePluginComponentExample }
-  }
+    const context = {
+        ignite: { removeModule,
+            removePluginComponentExample },
+    };
 
-  await plugin.remove(context)
+    await plugin.remove(context);
 
-  t.true(removeModule.calledWith('react-native-MODULENAME', { unlink: true }))
-  t.true(removePluginComponentExample.calledWith('NativeNavigationExample.js'))
-})
+    t.true(removeModule.calledWith('react-native-MODULENAME', { unlink: true }));
+    t.true(removePluginComponentExample.calledWith('NativeNavigationExample.js'));
+});
